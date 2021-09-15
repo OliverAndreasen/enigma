f87900b81fb685ca420d94b8386c1c6f74b93252
OliverAndreasen <121212Fhb44ypv>
2021-09-15 14:59:59 +0200

    code cleaning

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,7 +9,7 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
         // repeat always - until system.exit
-        while(true) {
+        while (true) {
             System.out.println("Velkommen til Enigma-projektet!");
             System.out.println("Vælg mellem en form for kryptering:");
             System.out.println(" 1) Caesar cipher");
@@ -20,7 +20,7 @@ public class Main {
             if (type == 1) {
                 System.out.println("Caesar cipher");
                 System.out.println("-------------");
-            }else if (type == 0) {
+            } else if (type == 0) {
                 System.out.println("Du har afsluttet programmet");
                 System.exit(0);
             }
@@ -33,16 +33,14 @@ public class Main {
             } else if (type == 1 && mode == 'd') {
                 decryptCaesarMenu();
             }
-    }
+        }
 
     }
 
     public static int letterToNumber(char letter) {
-
         String alfabet = " ABCDEFGHIJKLMNOPQRSTUVWXYZÆØÅ";
         int number = alfabet.indexOf(letter);
         return number;
-
     }
 
     public static int[] textToListOfNumbers(String text) {
@@ -62,12 +60,10 @@ public class Main {
     }
 
     public static String listOfNumbersToText(int[] numbers) {
-
         String text = "";
 
         for (int i = 0; i < numbers.length; i++) {
             int number = numbers[i];
-
             char letter = numberToLetter(number);
             text = text + letter;
         }
@@ -86,7 +82,7 @@ public class Main {
         //Omskriver positiv til negativ tal
         shift = -shift;
         // kalder caesarDecrypt med ciphertext og shift-værdi
-        String deCryptText = caesarDecrypt(cipherText,shift);
+        String deCryptText = caesarDecrypt(cipherText, shift);
         // udskriver plaintext modtaget fra ovenstående
         System.out.println(deCryptText);
     }
@@ -100,68 +96,47 @@ public class Main {
         System.out.println("Indtast shift-værdi");
         int shift = sc.nextInt();
         // kalder caesarEncrypt med ciphertext og shift-værdi
-        String encryptedText = caesarEncrypt(text,shift);
+        String encryptedText = caesarEncrypt(text, shift);
         // udskriver ciphertext modtaget fra ovenstående
         System.out.println(encryptedText);
     }
 
 
-    public static int[] shiftListOfNumbers(int[] numbers, int shift ) {
-
+    public static int[] shiftListOfNumbers(int[] numbers, int shift) {
         int[] shiftNumbers = new int[numbers.length];
-
-
         for (int i = 0; i < numbers.length; i++) {
-
             shiftNumbers[i] = shiftNumber(numbers[i], shift);
-
         }
-
         return shiftNumbers;
     }
 
-    public static int shiftNumber(int number, int shift ) {
-
-        int shiftNumber = number+shift;
-
+    public static int shiftNumber(int number, int shift) {
+        int shiftNumber = number + shift;
         if (shiftNumber > 29) {
             shiftNumber = Math.abs(shiftNumber - 29);
+        } else if (shiftNumber <= 0) {
+            // omskriver negativt tal til et positvt tal
+            int positiv = Math.abs(shiftNumber);
+            shiftNumber = Math.abs(29 - positiv);
         }
-        else if (number == 0) {
 
+        if (number == 0) {
             shiftNumber = 0;
-
-        }
-
-        else if (shiftNumber <= 0) {
-            //shiftNumber = Math.abs(29-shiftNumber);
-
-            if (shiftNumber == 0)
-            {
-                int positiv = Math.abs(shiftNumber);
-                shiftNumber = Math.abs(29-positiv);
-            }
-            else{
-
-                int positiv = Math.abs(shiftNumber);
-                shiftNumber = Math.abs(29-positiv);
-
-            }
         }
         return shiftNumber;
     }
 
-    public static String caesarDecrypt( String ciphertext, int shift ) {
+    public static String caesarDecrypt(String ciphertext, int shift) {
         ciphertext = ciphertext.toUpperCase();
         int[] listOfNumbers = textToListOfNumbers(ciphertext);
-        int[] shiftListOfNumbers = shiftListOfNumbers(listOfNumbers,shift);
+        int[] shiftListOfNumbers = shiftListOfNumbers(listOfNumbers, shift);
         String result = listOfNumbersToText(shiftListOfNumbers);
 
         return result;
     }
 
 
-    public static String caesarEncrypt(String plaintext, int shift ) {
+    public static String caesarEncrypt(String plaintext, int shift) {
         plaintext = plaintext.toUpperCase();
         // textToListOfNumbers
         int[] textToListOfNumbers = textToListOfNumbers(plaintext);
