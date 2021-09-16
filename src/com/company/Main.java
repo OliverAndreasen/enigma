cc5496b56388df4115a2c7bcd12cbd583c875802
OliverAndreasen <oliv2058@stud.kea.dk>
2021-09-16 20:32:30 +0200

    updates

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-
+           // Test
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // repeat always - until system.exit
